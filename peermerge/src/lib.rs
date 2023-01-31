dee2079d8b1e2b8e4e2577d6b8ea421f127144b2
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-01-31 13:23:17 +0200

    Re-export interface types

--- peermerge/src/lib.rs
+++ peermerge/src/lib.rs
@@ -10,3 +10,7 @@ pub use crate::core::Peermerge;
 pub use crate::repository::Repository;
 pub use ::automerge::*;
 pub use hypercore_protocol::{Protocol, ProtocolBuilder};
+#[cfg(not(target_arch = "wasm32"))]
+pub use random_access_disk::RandomAccessDisk;
+pub use random_access_memory::RandomAccessMemory;
+pub use random_access_storage::RandomAccess;
