bfe1c595a706e5dfd35148634cb909e56865deed
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-08-14 14:25:03 +0300

    Added tests to split and shrink

--- peermerge/src/common/entry.rs
+++ peermerge/src/common/entry.rs
@@ -3,7 +3,7 @@ use automerge::Change;
 use super::constants::PEERMERGE_VERSION;
 
 /// Content of an entry stored to a hypercore.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub(crate) enum EntryContent {
     /// First message of the doc feed.
     InitDoc {
@@ -60,7 +60,7 @@ impl EntryContent {
 }
 
 /// A document is stored in pieces to hypercores.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub(crate) struct Entry {
     pub(crate) version: u8,
     pub(crate) content: EntryContent,
@@ -174,7 +174,9 @@ pub(crate) fn split_datas_into_entries(
                 None
             }
         } else {
-            // The chunks fit exactly, add all user chunks into separate entries
+            // The chunks fit exactly, add all user chunks into separate entries,
+            // and the last meta chunk also to entry contents
+            entry_contents.push((Some(last_meta_chunk), None));
             Some(user_doc_data.to_vec())
         };
         if let Some(rest_of_user_doc_data) = rest_of_user_doc_data {
@@ -264,8 +266,9 @@ pub(crate) fn shrink_entries(mut entries: Vec<Entry>) -> (Vec<Entry>, u64) {
         } else if entries.len() > 1 {
             let doc_part: bool = matches!(&entries[1].content, EntryContent::DocPart { .. });
             if doc_part {
+                // Drain entries from after the init entry
                 let part_entry = entries
-                    .drain(i..i + 1)
+                    .drain(1..2)
                     .collect::<Vec<Entry>>()
                     .into_iter()
                     .next()
@@ -334,4 +337,159 @@ fn is_init_entries(entries: &Vec<Entry>) -> bool {
         entries[0].content,
         EntryContent::InitDoc { .. } | EntryContent::InitPeer { .. }
     )
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn entries_split_and_shrink() -> anyhow::Result<()> {
+        let small_meta_doc_data: Vec<u8> = vec![0; 4];
+        let small_user_doc_data: Vec<u8> = vec![1; 4];
+
+        let big_meta_doc_data: Vec<u8> = vec![2; 16];
+        let big_user_doc_data: Vec<u8> = vec![3; 16];
+
+        // Both fit exactly
+        let entries = split_datas_into_entries(
+            &small_meta_doc_data,
+            &Some(small_user_doc_data.clone()),
+            true,
+            8,
+        );
+        assert_eq!(
+            entries,
+            vec![Entry::new_init_doc(
+                0,
+                small_meta_doc_data.clone(),
+                Some(small_user_doc_data.clone()),
+            )],
+        );
+        assert_eq!(shrink_entries(entries.clone()), (entries, 0));
+
+        // Both exactly to two different parts
+        let entries = split_datas_into_entries(
+            &small_meta_doc_data,
+            &Some(small_user_doc_data.clone()),
+            true,
+            4,
+        );
+        assert_eq!(
+            entries,
+            vec![
+                Entry::new_init_doc(1, small_meta_doc_data.clone(), None),
+                Entry::new_doc_part(0, None, Some(small_user_doc_data.clone()))
+            ],
+        );
+        assert_eq!(
+            shrink_entries(entries),
+            (
+                vec![Entry::new_init_doc(
+                    1,
+                    small_meta_doc_data.clone(),
+                    Some(small_user_doc_data.clone())
+                )],
+                1
+            ),
+        );
+
+        // Meta overflows
+        let entries = split_datas_into_entries(
+            &big_meta_doc_data,
+            &Some(small_user_doc_data.clone()),
+            false,
+            10,
+        );
+        assert_eq!(
+            entries,
+            vec![
+                Entry::new_init_peer(1, big_meta_doc_data[..10].to_vec(), None),
+                Entry::new_doc_part(
+                    0,
+                    Some(big_meta_doc_data[10..].to_vec()),
+                    Some(small_user_doc_data.clone())
+                )
+            ],
+        );
+        assert_eq!(
+            shrink_entries(entries),
+            (
+                vec![Entry::new_init_peer(
+                    1,
+                    big_meta_doc_data.clone(),
+                    Some(small_user_doc_data.clone())
+                )],
+                1
+            ),
+        );
+
+        // User overflows
+        let entries = split_datas_into_entries(
+            &small_meta_doc_data,
+            &Some(big_user_doc_data.clone()),
+            false,
+            10,
+        );
+        assert_eq!(
+            entries,
+            vec![
+                Entry::new_init_peer(
+                    1,
+                    small_meta_doc_data.clone(),
+                    Some(big_user_doc_data[..6].to_vec())
+                ),
+                Entry::new_doc_part(0, None, Some(big_user_doc_data[6..].to_vec()))
+            ],
+        );
+        assert_eq!(
+            shrink_entries(entries),
+            (
+                vec![Entry::new_init_peer(
+                    1,
+                    small_meta_doc_data.clone(),
+                    Some(big_user_doc_data.clone())
+                )],
+                1
+            ),
+        );
+
+        // Both overflow
+        let entries = split_datas_into_entries(
+            &big_meta_doc_data,
+            &Some(big_user_doc_data.clone()),
+            false,
+            5,
+        );
+        assert_eq!(
+            entries,
+            vec![
+                Entry::new_init_peer(6, big_meta_doc_data[..5].to_vec(), None),
+                Entry::new_doc_part(0, Some(big_meta_doc_data[5..10].to_vec()), None),
+                Entry::new_doc_part(1, Some(big_meta_doc_data[10..15].to_vec()), None),
+                Entry::new_doc_part(
+                    2,
+                    Some(big_meta_doc_data[15..16].to_vec()),
+                    Some(big_user_doc_data[0..4].to_vec())
+                ),
+                Entry::new_doc_part(3, None, Some(big_user_doc_data[4..9].to_vec())),
+                Entry::new_doc_part(4, None, Some(big_user_doc_data[9..14].to_vec())),
+                Entry::new_doc_part(5, None, Some(big_user_doc_data[14..16].to_vec())),
+            ],
+        );
+        assert_eq!(
+            shrink_entries(entries),
+            (
+                vec![Entry::new_init_peer(
+                    6,
+                    big_meta_doc_data,
+                    Some(big_user_doc_data)
+                )],
+                6
+            ),
+        );
+
+        Ok(())
+    }
 }
